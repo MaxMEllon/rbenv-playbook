9c7695af7525dd94cdcf12252dfc7f0764aaa125
MaxMEllon <maxmellon1994@gmail.com>
2016-09-10 00:09:39 +0900

    :bug: small fix in rbenv spec

--- spec/localhost/rbenv_playbook_spec.rb
+++ spec/localhost/rbenv_playbook_spec.rb
@@ -9,7 +9,7 @@ describe package('rbenv') do
 
   it 'expect ruby version 2.3.0' do
     out, err, _ = Open3.capture3('rbenv prefix')
-    ruby_version = out.split('/').last
+    ruby_version = out.split('/').last.chomp
     expect(ruby_version).to eq '2.3.0'
   end
 end
