f88c0094466be297bd30498367b0b363c6935df6
MaxMEllon <maxmellon1994@gmail.com>
2016-09-10 00:17:26 +0900

    :bug: Fix config of ci and spec

--- spec/localhost/rbenv_playbook_spec.rb
+++ spec/localhost/rbenv_playbook_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe package('rbenv') do
   it 'expect installed rbenv command' do
-    out, err, _ = Open3.capture3('rbenv --version')
+    out, err, _ = Open3.capture3('/usr/bin/env bash -lc "rbenv --version"')
     expect(out).not_to eq nil
     expect(err).to eq ''
   end
 
   it 'expect ruby version 2.3.0' do
-    out, err, _ = Open3.capture3('rbenv prefix')
+    out, err, _ = Open3.capture3('/usr/bin/env bash -lc "rbenv prefix"')
     ruby_version = out.split('/').last.chomp
     expect(ruby_version).to eq '2.3.0'
   end
